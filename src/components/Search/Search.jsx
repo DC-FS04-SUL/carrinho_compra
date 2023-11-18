64164130a26dc70957d8bb10f57eb252e076be07
Francisco Wallison <franciscowallison@gmail.com>
2023-11-18 09:21:49 -0300

    add: adicionando evento de form search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,27 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import "./Search.css";
 
 function Search() {
-  // const [count, setCount] = useState(0)
+  const [search, setSearch] = useState("")
+
+  const buscar = (e) => {
+    // reload na pagina
+    e.preventDefault();
+
+    
+    alert(search)
+  }
 
   return (
-    <form className="d-flex" role="search">
+    <form className="d-flex" role="search" onSubmit={buscar}>
       <input
         className="form-control me-2"
         type="search"
+        name="search"
         placeholder="Search"
         aria-label="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button className="btn btn-outline-success" type="submit">
         Search
